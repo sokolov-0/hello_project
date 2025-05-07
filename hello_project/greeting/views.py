ce7c1f5368d67a2ebe83973bcaf092e4171cd5e6
Андрей Соколов <andrejsokolov@MacBook-Air-Andrej.local>
2025-05-07 11:55:07 +0500

    2 commit

--- hello_project/greeting/views.py
+++ hello_project/greeting/views.py
@@ -1,15 +1,14 @@
 # greeting/views.py
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
 
 def hello_view(request):
     name = request.GET.get("name")
     message = request.GET.get("message")
 
-    # оба параметра переданы и совпадают?
-    if name == "Recruto" and message == "Давай дружить":
-        return HttpResponse(f"Hello {name}! {message}", content_type="text/plain; charset=utf-8")
+    # Если кто-то совсем забыл параметры — можно выдать 400 Bad Request
+    if not name or not message:
+        return HttpResponseBadRequest("Missing parameters: ?name=…&message=…")
 
-    # иначе показываем форму
-    return render(request, "greeting/form.html")
+    # Просто подставляем то, что пришло из GET
+    return HttpResponse(f"Hello {name}! {message}!", content_type="text/plain; charset=utf-8")
